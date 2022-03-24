3f8d5c6604e58fe333e5752a6afaa96c2a610b13
Rasvk <rasmusvedholmkrog@gmail.com>
2022-03-24 13:06:45 +0100

    HTML template change

--- components/TaskShow.vue
+++ components/TaskShow.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
     <h2>taskshow here</h2>
+    <h3 v-if="task">{{ task.title }}</h3>
+    <h5 v-if="task.deadlineDate">Deadline: {{ task.deadlineDate }}</h5>
+    <p v-if="task">{{ task.description }}</p>
+    <h6 v-if="task.createdDate">
+      Created: {{ task.createdDate }} by {{ task.createdBy.name }}
+    </h6>
   </div>
 </template>
 
 <script>
-export default {}
+import { mapState } from 'vuex'
+
+export default {
+  computed: mapState({ task: (state) => state.task.task })
+}
 </script>
 
 <style scoped></style>
