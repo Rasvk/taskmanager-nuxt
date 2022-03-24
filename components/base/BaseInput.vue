1697fe195568b2679ec29d91f4d91775cb3b8b7b
Rasvk <rasmusvedholmkrog@gmail.com>
2022-03-24 15:04:14 +0100

    added templates to input, select

---
+++ components/base/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="field">
+    <label v-if="label"> {{ label }}</label>
+    <input id="label" :value="value" v-bind="$attrs" @input="updateValue" />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    updateValue(task) {
+      this.$emit('input', task.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
---
+++ components/base/BaseSelect.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select :id="label" :value="value" v-bind="$attrs" @input="updateValue">
+      <option value="" disabled selected hidden>Please Choose...</option>
+      <option
+        v-for="option in options"
+        :key="option"
+        :selected="option === value"
+        :value="option"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    }
+  },
+  methods: {
+    updateValue(task) {
+      this.$emit('input', task.target.value)
+    }
+  }
+}
+</script>
+
+<style scoped></style>
